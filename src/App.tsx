1caa236b97c45e7c8bd05642308ceddab963624c
jazib-zaidi <jazib@feedops.com>
2025-04-02 02:21:07 +0530

    fix failing build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Magento from './components/megento';
+import Magento from './components/Megento';
 import Home from './components/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
